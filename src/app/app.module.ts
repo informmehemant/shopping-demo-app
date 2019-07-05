ae87220fca1bc3869364efbfa06a99c8d5a04496
Hemant Singh <hemant.kumar@moonraft.com>
2019-07-05 13:04:20 +0530

    initial commit for setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule , NoopAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -9,7 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
+    NoopAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
